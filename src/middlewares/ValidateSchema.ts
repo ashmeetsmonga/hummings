2ad34c658bf29e5401a10fb46fcf6d20863964af
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-06-28 23:14:06 +0530

    added user create validation

---
+++ src/middlewares/ValidateSchema.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+import Joi, { ObjectSchema } from "joi";
+import { IUser } from "../models/User";
+
+export const validateSchema = (schema: ObjectSchema) => {
+	return async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			await schema.validateAsync(req.body);
+			next();
+		} catch (error) {
+			return res.status(422).json({ message: error });
+		}
+	};
+};
+
+export const Schemas = {
+	user: {
+		create: Joi.object<IUser>({
+			username: Joi.string().required(),
+			email: Joi.string().required(),
+			password: Joi.string().required(),
+		}),
+	},
+};
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { registerUser } from "../controllers/userController";
+import { Schemas, validateSchema } from "../middlewares/ValidateSchema";
 
 const userRouter = express.Router();
 
-userRouter.route("/register").post(registerUser);
+userRouter.route("/register").post(validateSchema(Schemas.user.create), registerUser);
 
 export default userRouter;
